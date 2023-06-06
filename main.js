f8f4d8a560418f1962d66bd265815bf5a9d52258
anntasi <anntasi881111@gmail.com>
2023-06-06 11:03:00 +0800

    b

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ let cityData = [
 let daysData = ['', 1, 2, 3, 4, 5
 
 ];
-let luggage=0;
-let check1=0;
-let check2=0;
+let luggage = 0;
+let check1 = 0;
+let check2 = 0;
 let day_tempmin = [];
 let day_tempmax = [];
 days = document.getElementById("daysSelect");
@@ -25,10 +25,10 @@ $(function () {
 			`<option value='${x}'>${daysData[x]}</option>`
 		);
 	}
-	
+
 	$("#citySelect").on("change", loadServerData);
 	$("#daysSelect").on("change", loadServerData);
-	$("[type = 'checkbox']").on("change",updateProgress);//任何checkbox有change就呼叫updateprogress
+	$("[type = 'checkbox']").on("change", updateProgress);//任何checkbox有change就呼叫updateprogress
 });
 
 function loadServerData() {
@@ -74,15 +74,15 @@ function loadServerData() {
 			   <img src='https://openweathermap.org/img/wn/${data.list[i+7].weather[0].icon}@2x.png'>${data.list[i+7].weather[0].description}
 			   <img src='https://openweathermap.org/img/wn/${data.list[i+8].weather[0].icon}@2x.png'>${data.list[i+8].weather[0].description}</p>`
 		   );*/
-		   let luggage=0;
+			let luggage = 0;
 			for (let i = 0; i < days.value; i++) {
 				let tem_min = 1000, tem_max = -10;
 				let rain = 0;
-				
+
 				for (let k = 0; k < 8; k++) {
 					if (data.list[8 * i + k].main.temp_min < tem_min) { tem_min = data.list[8 * i + k].main.temp_min; }
 					if (data.list[8 * i + k].main.temp_max > tem_max) { tem_max = data.list[8 * i + k].main.temp_max; }
-					if (data.list[i + 1].weather[0].icon == '10n') { rain = 1;luggage=1 }
+					if (data.list[i + 1].weather[0].icon == '10n') { rain = 1; luggage = 1 }
 				}
 
 				$("#result").append(
@@ -92,56 +92,55 @@ function loadServerData() {
 				if (rain == 1) {
 					$("#result").append(
 						`<p><img src='https://openweathermap.org/img/wn/10n@2x.png'></p>`);
-						
+
 				}
 				else {
 					$("#result").append(
 						`<p><img src='https://openweathermap.org/img/wn/02d@2x.png'></p>`);
-						
+
 				}
-				
-			}if(luggage==1&check1==0)
-				{	//deletee();
-					addelement("雨傘")
-					check1=1;}
-			else if(luggage==0&check2==0){
-				   //deletee();
-					addelement("防曬");
-					check2=1
-				}
+
+			} if (luggage == 1 & check1 == 0) {	//deletee();
+				addelement("雨傘")
+				check1 = 1;
+			}
+			else if (luggage == 0 & check2 == 0) {
+				//deletee();
+				addelement("防曬");
+				check2 = 1
+			}
 
 		})
 		.fail(function () { console.log("Error") })
 		.always(function () { console.log("Always") });
 
-}function updateProgress() {
-    let hasChecked = 0;
-    for(let x=0; x < $("[type='checkbox']").length; x++){
-        if($("[type='checkbox']")[x].checked){
-            hasChecked += 1;
-           
-        }
-    }
-    // $("meter").attr("min",0);
-    $("meter").attr("max", $("[type='checkbox']").length);
-    $("meter").attr("value", hasChecked);
+} function updateProgress() {
+	let hasChecked = 0;
+	for (let x = 0; x < $("[type='checkbox']").length; x++) {
+		if ($("[type='checkbox']")[x].checked) {
+			hasChecked += 1;
+
+		}
+	}
+	// $("meter").attr("min",0);
+	$("meter").attr("max", $("[type='checkbox']").length);
+	$("meter").attr("value", hasChecked);
 }
-function addelement(thing)
-{  var container=document.getElementById("container");
-    var newbox=document.createElement("input");
-    newbox.type="checkbox";
-	var label=document.createElement("label")
-	label.htmlFor=thing
-    var br=document.createElement("br");
-    label.appendChild(document.createTextNode(thing));
-   container.appendChild(newbox);
-   container.appendChild(label);
-   container.appendChild(br)
-   console.log("add");
+function addelement(thing) {
+	var container = document.getElementById("container");
+	var newbox = document.createElement("input");
+	newbox.type = "checkbox";
+	var label = document.createElement("label")
+	label.htmlFor = thing
+	var br = document.createElement("br");
+	label.appendChild(document.createTextNode(thing));
+	container.appendChild(newbox);
+	container.appendChild(label);
+	container.appendChild(br)
+	console.log("add");
 }
-function deletee()
-{
-	var container=document.getElementById("container");
+function deletee() {
+	var container = document.getElementById("container");
 	container.empty();
 	console.log("del");
 }
